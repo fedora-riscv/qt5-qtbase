f0e6f2d2990135535dfc97191933040dfcfa4d1b
Rex Dieter <rdieter@math.unl.edu>
2016-03-24 12:40:47 -0500

    changed mind, check only for opengl 1

--- 10-qt5-check-opengl2.sh
+++ 10-qt5-check-opengl2.sh
@@ -4,7 +4,7 @@ if [ -z "$QT_XCB_FORCE_SOFTWARE_OPENGL" ]; then
 
 QT5_CHECK_OPENGL_VERSION=`LANG=C glxinfo 2> /dev/null | grep '^OpenGL version string: ' | head -n 1 | sed -e 's/^OpenGL version string: \([0-9]\).*$/\1/g'` ||:
 
-if [ "$QT5_CHECK_OPENGL_VERSION" == "1" -o "$QT5_CHECK_OPENGL_VERSION" == "0" ]; then
+if [ "$QT5_CHECK_OPENGL_VERSION" == "1" ]; then
   QT_XCB_FORCE_SOFTWARE_OPENGL=1
   export QT_XCB_FORCE_SOFTWARE_OPENGL
 fi
